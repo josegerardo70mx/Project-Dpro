2d43848f40b6e589d63e4f37277cc4a6051c9afa
OEM Configuration (temporary user) <josearguelles@live.com.mx>
2023-03-07 13:52:09 -0600

    added header input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
+import React, {useState} from "react";
+
+
+
+
 export default function Home() {
+  const [val, setVal] = useState('Que te gustaria ver')
+
+  const Enviar = () => {
+   alert(val)
+  }
+  const change = event => {
+   setVal(event.target.value)
+
+  }
+
+
+
+
   return (
     <>
       <div className="header">
-        
-              <h1 className="text-3xl font-extrabold text-white padding-top: 5rem;" >PARA CUANDO?</h1>
-      </div>
+        <h1>PARA CUANDO?</h1>
+
+        <input value= {val} className="text-black text-md" onChange={change} /><br/>
+        <button onClick={Enviar}  >Enviar</button>
+
+
+
+        </div>
     </>
   );
 }
